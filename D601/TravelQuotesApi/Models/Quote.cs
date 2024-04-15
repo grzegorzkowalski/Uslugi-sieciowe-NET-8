01642ea0d08fd463d1a8f90c24803348f1ae3ff9
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-04-15 17:34:23 +0200

    Quotes

---
+++ D601/TravelQuotesApi/Models/Quote.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelQuotesApi.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Message { get; set; }
+    }
+}
