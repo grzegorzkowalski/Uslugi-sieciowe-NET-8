f6b6df1fcf2391af4f3483c9771443e8ff4d8002
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-05-06 16:45:04 +0200

    add repository and controllers

---
+++ D601/BlogCMS/Controllers/PostsController.cs
@@ -0,0 +1,83 @@
+﻿using BlogCMS.IRepositories;
+using BlogCMS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IRepository<Post> _postRepository;
+
+        public PostsController(IRepository<Post> postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        // GET: api/posts
+        [HttpGet]
+        public async Task<IActionResult> GetAllPosts()
+        {
+            var posts = await _postRepository.GetAllAsync();
+            return Ok(posts);
+        }
+
+        // GET: api/posts/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPostById(int id)
+        {
+            var post = await _postRepository.GetByIdAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        // POST: api/posts
+        [HttpPost]
+        public async Task<IActionResult> CreatePost([FromBody] Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newPostId = await _postRepository.AddAsync(post);
+            return CreatedAtAction(nameof(GetPostById), new { id = newPostId }, post);
+        }
+
+        // PUT: api/posts/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePost(int id, [FromBody] Post post)
+        {
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+
+            var result = await _postRepository.UpdateAsync(post);
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/posts/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var result = await _postRepository.DeleteAsync(id);
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ D601/BlogCMS/Repositories/EfCoreRepository.cs
@@ -0,0 +1,66 @@
+﻿using BlogCMS.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCMS.Repositories
+{
+    public class EfCoreRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private DbSet<T> _entities;
+
+        public EfCoreRepository(DbContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _entities.ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id) => await _entities.FindAsync(id);
+
+        public async Task<int> AddAsync(T entity)
+        {
+            try
+            {
+                await _entities.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                //return (int)_context.Entry(entity).Property("Id").CurrentValue;
+                return 1;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }   
+        }
+
+        public async Task<bool> UpdateAsync(T entity)
+        {
+            try
+            {
+                _entities.Update(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                var entity = await GetByIdAsync(id);
+                _entities.Remove(entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }            
+        }
+    }
+}
