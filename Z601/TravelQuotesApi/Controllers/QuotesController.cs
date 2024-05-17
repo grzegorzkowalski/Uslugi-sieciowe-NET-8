7a4247a2d572bfe6d18264e1648e775f0ff1e2b7
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-05-17 19:08:18 +0200

    add crud for quotes

---
+++ Z601/TravelQuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelQuotesApi.Data;
+using TravelQuotesApi.Models;
+
+namespace TravelQuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Quotes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Quote>>> GetQuotes()
+        {
+            return await _context.Quotes.ToListAsync();
+        }
+
+        // POST: api/Quotes
+        [HttpPost]
+        public async Task<ActionResult<Quote>> PostQuote(Quote quote)
+        {
+            _context.Quotes.Add(quote);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuote", new { id = quote.Id }, quote);
+        }
+
+        // Dodaj więcej metod API według potrzeb...
+    }
+}
