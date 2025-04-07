50d71d9a2b3f8fbd61337e9d9b7708d7a04124c1
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2025-04-07 17:47:38 +0200

    add quotes controller

---
+++ D601_2025/BlogCMS/Controllers/QuotesController.cs
@@ -0,0 +1,35 @@
+﻿using BlogCMS.Data;
+using BlogCMS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Quotes
+        [HttpPost]
+        public async Task<ActionResult<Quote>> PostQuote(Quote quote)
+        {
+            _context.Quotes.Add(quote);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuote", new { id = quote.Id }, quote);
+        }
+
+        [HttpGet]
+        public Quote GetQuote(int id)
+        {
+            return _context.Quotes.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
